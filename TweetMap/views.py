29a4965fcd8e03ede8dd5b66618c04080498ca6c
tanaylakhani <tpl253@nyu.edu>
2014-12-09 18:52:22 -0500

    counts

--- TweetMap/views.py
+++ TweetMap/views.py
@@ -10,14 +10,19 @@ from django.template import RequestContext
 from notifications.models import Notification
 from notifications import notify
 from django.core.urlresolvers import reverse
+from rest_framework.response import Response
 
 from django.contrib.staticfiles.storage import staticfiles_storage
    
 def home(request):
-    tweets = TwitterServiceTwitterfeed2.objects.all().filter(x__isnull = False,y__isnull = False)[0:20000]
+    tweets = TwitterServiceTwitterfeed2.objects.all().filter(x__isnull = False,y__isnull = False).order_by('-id')[0:20000]
     tweet_count = TwitterServiceTwitterfeed2.objects.all().filter(x__isnull = False,y__isnull = False).count()
     context = {'tweets' : tweets,'tweet_count': tweet_count}
     return render_to_response("tweet-map.html", context, RequestContext(request))
 
 def tweet_sync(request):
     return render(request,"tweet-sync.html", locals())
+def tweet_count(request):
+    tweet_count = TwitterServiceTwitterfeed2.objects.all().filter(x__isnull = False,y__isnull = False).count()
+    data = {'tweet_count':tweet_count}
+    return Response({"success":True,"error":None,"data":data})
